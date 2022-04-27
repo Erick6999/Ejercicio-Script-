e205a488899dd06485867d7ce7abb16a10f6062e
Erick6999 <18670125@iguala.tecnm.mx>
2022-04-27 08:47:10 -0500

    Primeros scripts

---
+++ main.js
@@ -0,0 +1,28 @@
+/*var num = prompt('Ingresa un numero:');
+
+function squareMyNumber(num){
+    return num ** 2
+}
+
+let squared = squareMyNumber(num);
+alert("El numero es = " +num);*/
+
+
+/*alert("Hola Js");
+var num = prompt('Ingresa un numero:');
+
+const  cuadrado = function (num){
+    return num * num;
+}
+alert("El numero es: "+ num);*/
+
+let nome = prompt("¿Cuál es tu nombre?");
+
+let nombre = document.getElementById("nombre");
+nombre.textContent ="Hola "+ nome;
+/*const f = () => {
+    return 'hola'+ nome;
+};
+alert(f);*/
+
+
